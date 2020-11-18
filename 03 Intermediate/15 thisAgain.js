4f582cf44329d451201f5c47e2d6cf2df93521de
ramachandra <bramachandra99@gmail.com>
2020-11-18 16:37:33 +0530

    New files added

---
+++ 03 Intermediate/15 thisAgain.js
@@ -0,0 +1,23 @@
+// For all regular function calls .this refers to window object
+console.log(this);
+var user = {
+    firstName : "Ram",
+    courseCount : 4,
+    getCourseCount : function () {
+        console.log("This is line 7", this);
+        function sayHello(){
+            console.log("Hello");
+            console.log("This is line 10",this);
+        }
+        sayHello();
+    },
+};
+
+user.getCourseCount();    // not a regular function call :  This is through an object
+
+function sayHello() {
+    console.log("Hello");
+}
+
+sayHello();     // This is a function call
+
